6c60dbce3e1ff780bae754de46ace0213c4315e9
Yaxiang <liuyaxiang123@icloud.com>
2020-10-15 09:07:37 +0800

    添加监控区

--- js/flexible.js
+++ js/flexible.js
@@ -13,9 +13,9 @@
   }
   setBodyFontSize();
 
-  // set 1rem = viewWidth / 10
+  // set 1rem = viewWidth / 20
   function setRemUnit() {
-    var rem = docEl.clientWidth / 20
+    var rem = docEl.clientWidth / 24
     docEl.style.fontSize = rem + 'px'
   }
 
